3bbc15bbacadd4816cda6cb3ba33fe6e0fb92b2b
Ghsoon <132147874+Ghsoon@users.noreply.github.com>
2023-04-30 19:24:42 +0300

    Add files via upload

---
+++ EvangelionClock.h
@@ -0,0 +1,9 @@
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface EvangelionClock : ScreenSaverView
+{
+    IBOutlet id configSheet;
+    IBOutlet id styleOption;
+    IBOutlet id screenDisplayOption;
+}
+@end
